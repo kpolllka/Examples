865b726a36f8ff1ce151424322d785daf048c3c7
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-04 20:37:13 +0700

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// Имеется одномерный массив array из n элементов,
+// требуется найти элемент массива, равный find
+
+int[] array = {61, 82, 93, 41, 55, 6, 93, 77, 83};
+
+int n = array.Length;
+int find = 93;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index ++;
+}
