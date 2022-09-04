22b63ee6e7dd7af6a4a0a2ff2643a5cdfb0154f7
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-04 21:17:03 +0700

    Examples011_ArrayLibrary

---
+++ Examples011_ArrayLibrary/Program.cs
@@ -0,0 +1,57 @@
+﻿//сделать заполнение массива на 10 индексов рандомными числами
+//и вывести его на экран
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        //index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}    
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4; //принудительно добавили число 4 на 4ю и 6ю позиции массива
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+if (pos == -1)
+{
+    Console.WriteLine("Запрашиваемое число отсутствует в массиве");
+}
+else Console.WriteLine(pos);
